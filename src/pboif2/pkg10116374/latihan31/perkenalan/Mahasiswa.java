2f45d884beb7adb6a9cfa8f869e81ac608078e93
mortdecai40 <rizkinabil.999@gmail.com>
2020-11-10 18:40:43 +0700

    Latihan 31

---
+++ src/pboif2/pkg10116374/latihan31/perkenalan/Mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10116374.latihan31.perkenalan;
+
+/**
+ *
+ * @author Acromyrmex
+ */
+class Mahasiswa {
+      public String nama;
+    public String nim;
+        
+    public void perkenalkanDiri() {
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM " + nim);
+        System.out.println("My Name is " + nama);
+        System.out.println("");
+    
+}
+    
+}
---
+++ src/pboif2/pkg10116374/latihan31/perkenalan/PBOIF210116374Latihan31Perkenalan.java
@@ -0,0 +1,42 @@
+package pboif2.pkg10116374.latihan31.perkenalan;
+
+/**
+ * @author Acromyrmex
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk meperkenalkan diri
+ *
+ */
+public class PBOIF210116374Latihan31Perkenalan {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.nim = "10116374";
+        mhs.nama = "Teuku Rizki Nabil";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10116372";
+        mhs.nama = "Farhan Rizki";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10116373";
+        mhs.nama = "Roihan Pratama";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10115574";
+        mhs.nama = "Faele Antoni";
+        
+        mhs.perkenalkanDiri();
+           
+    }
+
+    
+    
+    
+}
